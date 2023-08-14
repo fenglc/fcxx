10a309f825dccdaae8cf84c14eef4517c0d8f9a8
fenglc <fenglc89@gmail.com>
2023-08-14 03:09:34 +0100

    build for windows

---
+++ tests/land/time_ms.cpp
@@ -0,0 +1,35 @@
+#include "time_ms.h"
+
+#ifdef _WIN32
+#include <time.h>
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+
+using namespace land;
+
+time_ms land::get_time_ms()
+{
+#ifdef _WIN32
+    SYSTEMTIME lt;
+    GetLocalTime(&lt);
+
+    tm tm;
+    tm.tm_year = lt.wYear - 1900;
+    tm.tm_mon = lt.wMonth - 1;
+    tm.tm_mday = lt.wDay;
+    tm.tm_hour = lt.wHour;
+    tm.tm_min = lt.wMinute;
+    tm.tm_sec = lt.wSecond;
+    tm.tm_isdst = -1;
+
+    timeval tv{};
+    time_t sec = mktime(&tm);
+    return (sec * 1000) + lt.wMilliseconds;
+#else
+    timeval tv{};
+    gettimeofday(&tv, nullptr);
+    return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
+#endif
+}
--- tests/land/time_ms.h
+++ tests/land/time_ms.h
@@ -1,20 +1,13 @@
 #ifndef _LAND_TIME_MS_H
 #define _LAND_TIME_MS_H
 
-#include <sys/time.h>
-
 namespace land
 {
     typedef long int time_ms;
 
-    time_ms get_time_ms()
-    {
-        timeval tv{};
-        gettimeofday(&tv, nullptr);
-        return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-    }
+    time_ms get_time_ms();
 
-    double diff_time_ms(time_ms end, time_ms begin)
+    inline double diff_time_ms(time_ms end, time_ms begin)
     {
         return end - begin;
     }
